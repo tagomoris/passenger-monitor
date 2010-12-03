627ac25b5f88a0365a96c57e1443150e0945c1fd
TAGOMORI Satoshi <tagomoris@livedoor.jp>
2010-12-03 15:31:06 +0900

    add idle

--- passenger-monitor.rb
+++ passenger-monitor.rb
@@ -14,33 +14,32 @@ class PassengerMonitor
   def status_text(server)
     status = server.connect(:passenger_status){server.stats}
     [["Busy Processes (Active): ", status.active],
+     ["Idle Processes: ", status.count - status.active],
      ["Total Processes (Count): ", status.count],
      ["Max Processes: ", status.max],
      ["Global Queue Size: ", status.global_queue_size]].map{|l,v| l + v.to_i.to_s}.join("\n")
   end
 
   def call(env)
-    if env['PATH_INFO'] == @path
-      source = IPAddr.new(env['REMOTE_ADDR'])
-      if @allow.inject(false){|r,a| r or a.include?(source)}
-        servers = PhusionPassenger::AdminTools::ServerInstance.list
-        if servers.size < 1
-          [500, {}, ["passenger not running."]]
-        elsif servers.size > 1
-          env['QUERY_STRING'] =~ /pid=(\d+)/
-          if $1 and PhusionPassenger::AdminTools::ServerInstance.for_pid($1.to_i)
-            [200, {}, [status_text(PhusionPassenger::AdminTools::ServerInstance.for_pid($1.to_i))]]
-          else
-            [200, {}, ["<html><body><ul>" + servers.map{|s| "<li><a href='#{@path}?pid=#{s.pid}'>pid: #{s.pid}</a></li>" }.join]]
-          end
+    return @app.call(env) if env['PATH_INFO'] == @path
+
+    source = IPAddr.new(env['REMOTE_ADDR'])
+    if @allow.inject(false){|r,a| r or a.include?(source)}
+      servers = PhusionPassenger::AdminTools::ServerInstance.list
+      if servers.size < 1
+        [500, {}, ["passenger not running."]]
+      elsif servers.size > 1
+        env['QUERY_STRING'] =~ /pid=(\d+)/
+        if $1 and PhusionPassenger::AdminTools::ServerInstance.for_pid($1.to_i)
+          [200, {}, [status_text(PhusionPassenger::AdminTools::ServerInstance.for_pid($1.to_i))]]
         else
-          [200, {}, [status_text(servers.first)]]
+          [200, {}, ["<html><body><ul>" + servers.map{|s| "<li><a href='#{@path}?pid=#{s.pid}'>pid: #{s.pid}</a></li>" }.join]]
         end
       else
-        [403, {}, ["source ip forbidden"]]
+        [200, {}, [status_text(servers.first)]]
       end
     else
-      @app.call(env)
+      [403, {}, ["source ip forbidden"]]
     end
   end
 end
